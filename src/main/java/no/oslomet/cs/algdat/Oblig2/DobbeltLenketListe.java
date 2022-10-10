1a0fb8ccad15fc1c8f43759aaf7f32245dc634d6
Skjolden <eivind194@hotmail.com>
2022-10-10 20:49:03 +0200

    fikset nullstill(), manglet å fjerne siste antallverdi

--- src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
@@ -256,6 +256,7 @@ public class DobbeltLenketListe<T> implements Liste<T> {
             denne.verdi = null;
             hode = null;
             hale = null;
+            antall--;
             endringer++;
         } else {
             throw new IllegalArgumentException("Listen er allerede tom!");
